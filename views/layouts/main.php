f76af2cef07bbdf38e40883e9e5b644c41ade4e7
andrewchosen <andrewchosen@fullsail.edu>
2016-09-18 12:14:10 -0500

    More configuration for Yii integration

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -19,6 +19,7 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700" rel="stylesheet">
     <?php $this->head() ?>
 </head>
 <body>
